56050ba87845e58f86ec62bd3c789ae25d8b32a5
mhanik00 <mhanik063@gmail.com>
2024-04-08 21:23:03 +0600

    fix

---
+++ script.js
@@ -0,0 +1,17 @@
+// function newPage(){
+//     console.log("New page");
+//     const display=document.getElementById("display");
+//     display.innerHTML=`
+//     <label for="link"> add your link
+//     </label>
+//     <input type="url" id="link" placeholder="Enter your link">
+//     <button onclick="addLink()">Add Link</button>
+//     `;
+// }
+
+// function addLink(){
+//     var buttonUrl=document.getElementById("link").value;
+//     const linkBtn=document.getElementById("btn");
+//     linkBtn.location.href = buttonUrl;
+
+// }
