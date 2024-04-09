6b66d225aed4cf2deeb496832dac5c1f8cc16360
mhanik00 <mhanik063@gmail.com>
2024-04-09 16:19:18 +0600

    last commit all done

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
-// function newPage(){
-//     console.log("New page");
-//     const display=document.getElementById("display");
-//     display.innerHTML=`
-//     <label for="link"> add your link
-//     </label>
-//     <input type="url" id="link" placeholder="Enter your link">
-//     <button onclick="addLink()">Add Link</button>
-//     `;
-// }
 
-// function addLink(){
-//     var buttonUrl=document.getElementById("link").value;
-//     const linkBtn=document.getElementById("btn");
-//     linkBtn.location.href = buttonUrl;
+function addTask() {
+    const password= document.getElementById("password").value;
+   
+    if(
+        password==="admin123"
+    ){
+        const inputBox = document.getElementById("input-box");
+       
+                const result= document.getElementById("result");
+                result.innerHTML =
+                `<a href="${inputBox.value}"><button class="btn">click me<button></a>`;
+                
+                saveData()
+                inputBox.value = "";
+                password.value="";
+    }
 
-// }
+    else{
+        alert("log in failed");
+    }
+    // TASK input box
+   
+    
+}
+
+function saveData(){
+    localStorage.setItem("data", result.innerHTML);
+}
+
+function loadData(){
+    const data = localStorage.getItem("data");
+    result.innerHTML = data;
+}
+
+loadData();
